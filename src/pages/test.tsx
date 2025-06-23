974a6c5f57740a97daf258a73fe9495573185af1
ThienTM27 <ubw1212@gmail.com>
2025-06-24 02:02:20 +0700

    Update test

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState} from "react";
 import * as React from "react";
 
 interface userInfoType {
@@ -9,14 +9,22 @@ interface userInfoType {
 
 const Test = () => {
     const [userInfoList, setUserInfoList] = useState<userInfoType[]>([]);
+    const [fetchedUserList, setFetchedUserList] = useState<userInfoType[]>([]);
     const [formData, setFormData] = useState({
         name: "",
         birthdate: "",
         phone: ""
     });
 
+    const fetchUsersList = async () => {
+        const res = await fetch('https://685998ba9f6ef9611153a902.mockapi.io/api/userinfo');
+        const data = await res.json();
+        setFetchedUserList(data);
+    };
+
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
+        const {name, value} = e.target;
         setFormData(prev => ({
             ...prev,
             [name]: value
@@ -34,33 +42,74 @@ const Test = () => {
 
         setUserInfoList(prev => [...prev, newUser]);
 
-        setFormData({ name: "", birthdate: "", phone: "" });
+        setFormData({name: "", birthdate: "", phone: ""});
 
         console.log("Current list:", userInfoList);
     };
 
     return (
         <>
-            <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column" }}>
-                <label>Enter name:</label>
-                <input id='name-input' name="name" type="text" value={formData.name} onChange={handleChange} />
+            <div style={{display: "flex", justifyContent: "space-between", gap: 150}}>
+                <div style={{display: "flex", flexDirection: "column"}}>
+                    <h1>Form test</h1>
+                    <form onSubmit={handleSubmit} style={{display: "flex", flexDirection: "column"}}>
+                        <label>Enter name:</label>
+                        <input id='name-input' name="name" type="text" value={formData.name} onChange={handleChange}/>
 
-                <label>Enter birthdate:</label>
-                <input id='birthdate-input' name="birthdate" type="text" value={formData.birthdate} onChange={handleChange} />
+                        <label>Enter birthdate:</label>
+                        <input id='birthdate-input' name="birthdate" type="text" value={formData.birthdate}
+                               onChange={handleChange}/>
 
-                <label>Enter phone number:</label>
-                <input id='phone-input' name="phone" type="text" value={formData.phone} onChange={handleChange} />
+                        <label>Enter phone number:</label>
+                        <input id='phone-input' name="phone" type="text" value={formData.phone}
+                               onChange={handleChange}/>
 
-                <button id='submit-button' type="submit">Submit</button>
-            </form>
+                        <button id='submit-button' type="submit">Submit</button>
+                    </form>
 
-            <ul>
-                {userInfoList.map((user, idx) => (
-                    <li key={idx}>
-                        {user.name} - {user.birthdate} - {user.phone}
-                    </li>
-                ))}
-            </ul>
+                    <table id='local-user-list' border={1} cellPadding="8">
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Birthdate</th>
+                            <th>Phone</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {userInfoList.map((user, idx) => (
+                            <tr key={idx}>
+                                <td>{user.name}</td>
+                                <td>{user.birthdate}</td>
+                                <td>{user.phone}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+                <div style={{display: "flex", flexDirection: "column"}}>
+                    <h1>API interception test</h1>
+                    <button id='fetch-user-btn' onClick={() => fetchUsersList()}>Get users</button>
+                    <table id='fetched-user-list' border={1} cellPadding="8">
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Birthdate</th>
+                            <th>Phone</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {fetchedUserList.map((user, idx) => (
+                            <tr key={idx}>
+                                <td>{user.name}</td>
+                                <td>{user.birthdate}</td>
+                                <td>{user.phone}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
         </>
     );
 };
--- src/unit-test/test.ts
+++ src/unit-test/test.ts
@@ -1,32 +1,48 @@
-import {fixture, Selector} from 'testcafe'
+import { fixture, Selector, RequestLogger } from 'testcafe';
 
-fixture('Getting Started').page('http://localhost:5173/test')
+const simpleLogger = RequestLogger('https://685998ba9f6ef9611153a902.mockapi.io/api/userinfo', {
+    logRequestHeaders: true,
+    logResponseHeaders: true
+});
+
+fixture('Getting Started').page('http://localhost:5173/test').requestHooks(simpleLogger);
 
 test('User can submit the form and see their info', async t => {
-    await t.typeText('#name-input', 'Thien')
+    await t
+        .typeText('#name-input', 'Thien')
         .typeText('#birthdate-input', '27/10/2004')
         .typeText('#phone-input', '0123456789')
         .click('#submit-button')
-        .wait(2000)
+        .wait(500);
 
-    const listItems1 = Selector('ul li').withText('Thien')
+    const thienRow = Selector('#local-user-list tr').withText('Thien');
 
     await t
-        .expect(listItems1.exists).ok()
-        .expect(listItems1.innerText).contains('Thien')
-        .expect(listItems1.innerText).contains('27/10/2004')
+        .expect(thienRow.exists).ok()
+        .expect(thienRow.innerText).contains('27/10/2004')
+        .expect(thienRow.innerText).contains('123456789');
 
-    await t.typeText('#name-input', 'Khang')
-        .typeText('#birthdate-input', '04/03/2004')
-        .typeText('#phone-input', 'abcdef')
+    await t
+        .typeText('#name-input', 'Khang', { replace: true })
+        .typeText('#birthdate-input', '04/03/2004', { replace: true })
+        .typeText('#phone-input', 'abcdef', { replace: true })
         .click('#submit-button')
-        .wait(2000)
+        .wait(500);
 
-    const listItems2 = Selector('ul li').withText('Khang')
+    const khangRow = Selector('#local-user-list tr').withText('Khang');
 
     await t
-        .expect(listItems2.exists).ok()
-        .expect(listItems2.innerText).contains('Khang')
-        .expect(listItems2.innerText).contains('04/03/2004')
-        .expect(listItems2.innerText).notContains('NaN')
+        .expect(khangRow.exists).ok()
+        .expect(khangRow.innerText).contains('04/03/2004')
+        .expect(khangRow.innerText).match(/^\d+$/);
+});
+
+test('fetch user list from API', async t => {
+    await t.click('#fetch-user-btn').expect(simpleLogger.contains(r => r.response.statusCode === 200)).ok();
+
+    const req = simpleLogger.requests[0];
+    console.log('Request URL:', req.request.url);
+    console.log('Response status:', req.response.statusCode);
 })
+
+
